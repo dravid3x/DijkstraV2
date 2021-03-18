7b289ee45a5c8a209fe260d3d37b7a938624b904
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-03-18 09:18:32 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> matrice = new List<List<int>>();
+
+            List<int> temp = new List<int>();
+
+            Console.Write("Inserire il numero di nodi: ");
+            int nNodi = Convert.ToInt32(Console.ReadLine());
+            nNodi--;
+
+            for (int i = 0; i < nNodi; i++)
+            {
+                matrice.Add(temp);
+                matrice[i].Add(0);
+            }
+            
+            for (int i = 0; i < nNodi; i++)
+            {
+                for (int k = 0; k < nNodi; k++)
+                {
+                    Console.Write(matrice[i][k].ToString());
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
