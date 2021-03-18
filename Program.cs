f49027cdcf1d4c2c5d4d3e85d4692981038fea9e
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-03-18 10:11:48 +0100

    Inizio Inserimento

--- Program.cs
+++ Program.cs
@@ -3,24 +3,21 @@ using System.Collections.Generic;
 
 namespace Dijkstra
 {
-    class Program
+    public partial class Program
     {
-        static void Main(string[] args)
+        static int nNodi = 0;
+        static List<List<int>> matrice = new List<List<int>>();
+        public static void Main(string[] args)
         {
-            List<List<int>> matrice = new List<List<int>>();
-
             List<int> temp = new List<int>();
-
             Console.Write("Inserire il numero di nodi: ");
-            int nNodi = Convert.ToInt32(Console.ReadLine());
+            nNodi = Convert.ToInt32(Console.ReadLine());
             nNodi--;
-
             for (int i = 0; i < nNodi; i++)
             {
                 matrice.Add(temp);
                 matrice[i].Add(0);
             }
-            //
             for (int i = 0; i < nNodi; i++)
             {
                 for (int k = 0; k < nNodi; k++)
@@ -28,6 +25,34 @@ namespace Dijkstra
                     Console.Write(matrice[i][k].ToString());
                 }
                 Console.Write("\n");
+            }
+            InserimentoArchi();
+            for (int i = 0; i < nNodi; i++)
+            {
+                for (int k = 0; k < nNodi; k++)
+                {
+                    Console.Write(matrice[i][k].ToString());
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public static void InserimentoArchi()
+        {
+            int indice = 1;
+            for (int y = 0; y < nNodi; y++)
+            {
+                int tempX = 0;
+                for (int x = 0; x < nNodi; x++)
+                {
+                    if (tempX < indice) tempX++;
+                    else
+                    {
+                        Console.WriteLine("Arco " + y.ToString() + " -> " + x.ToString());
+                        matrice[y][x] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                indice++;
             }
         }
     }
