9bb9678f7dbdd839a663c0e14e0e3229914d75e6
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-03-24 18:11:25 +0100

    Prima Parte Fatta

--- Program.cs
+++ Program.cs
@@ -5,40 +5,33 @@ namespace Dijkstra
 {
     public partial class Program
     {
-        static int nNodi = 0;
-        static List<List<int>> matrice = new List<List<int>>();
+        struct s_item { public int indice; public int costo; }
+        static int nNodi = 0, posGenerale = 0;
+        static List<List<int>> matrice = new List<List<int>>(); //Lista contenente Liste di interi, il tutto crea una matrice dinamica
+        static List<s_item> PercorsoCosti = new List<s_item>();
         public static void Main(string[] args)
         {
-            List<int> temp = new List<int>();
             Console.Write("Inserire il numero di nodi: ");
             nNodi = Convert.ToInt32(Console.ReadLine());
-            nNodi--;
+            //Ciclo che inizializza le liste con nNodi elementi al loro interno
             for (int i = 0; i < nNodi; i++)
             {
-                matrice.Add(temp);
-                matrice[i].Add(0);
+                matrice.Add(new List<int>());
+                PercorsoCosti.Add(new s_item());
+                for(int k = 0; k < nNodi; k++) matrice[i].Add(0);
             }
-            for (int i = 0; i < nNodi; i++)
-            {
-                for (int k = 0; k < nNodi; k++)
-                {
-                    Console.Write(matrice[i][k].ToString());
-                }
-                Console.Write("\n");
-            }
+            Stampa();
             InserimentoArchi();
-            for (int i = 0; i < nNodi; i++)
-            {
-                for (int k = 0; k < nNodi; k++)
-                {
-                    Console.Write(matrice[i][k].ToString());
-                }
-                Console.Write("\n");
-            }
+            Stampa();
+
+            PercorsoCosti[posGenerale] = CalcolaArcoMinimo(0);
+            Console.WriteLine("Index: " + PercorsoCosti[posGenerale].indice + " Costo: " + PercorsoCosti[posGenerale].costo);
+            posGenerale++;
         }
 
         public static void InserimentoArchi()
         {
+            //Funzione che inserisce i costi dei collegamenti dei nodi all'interno della matrice
             int indice = 1;
             for (int y = 0; y < nNodi; y++)
             {
@@ -49,11 +42,45 @@ namespace Dijkstra
                     else
                     {
                         Console.WriteLine("Arco " + y.ToString() + " -> " + x.ToString());
-                        matrice[y][x] = Convert.ToInt32(Console.ReadLine());
+                        int val = Convert.ToInt32(Console.ReadLine());
+                        matrice[y][x] = val;
+                        matrice[x][y] = val;    //Evita di dover inserire collegamenti duplicati (A con B è uguale a B con A)
+                        //Stampa();
                     }
                 }
                 indice++;
             }
         }
+
+        private static void Stampa()
+        {
+            //Funzone di stampa della matrice
+            for (int i = 0; i < nNodi; i++)
+            {
+                for (int k = 0; k < nNodi; k++)
+                {
+                    Console.Write(matrice[i][k].ToString());
+                }
+                Console.Write("\n");
+            }
+        }
+        private static s_item CalcolaArcoMinimo(int indice)
+        {
+            s_item minore;
+            minore.costo = 9999999;
+            minore.indice = 0;
+            for(int i = indice + 1; i < nNodi; i++)
+            {
+                if (matrice[indice][i] < minore.costo)
+                {
+                    minore.costo = matrice[indice][i];
+                    minore.indice = i;
+                }
+            }
+            return minore;
+        }
     }
 }
+
+//Per fare l'"hop" semplicemente partendo dall'indice del nodo vedo a cosa è collegato (scorrendo l'indice x)
+//e mettendo il valore della x del nodo con costo minore come indice y si fà per l'appunto il salto al prossimo nodo
